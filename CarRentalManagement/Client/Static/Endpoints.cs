4b3e115e492b4be75e6c5a8b4fc03b98c5c16829
Eusouf 2202385G <2202385G@student.tp.edu.sg>
2023-12-10 17:16:20 +0800

    Added Blazor List Pages with Delete function

---
+++ CarRentalManagement/Client/Static/Endpoints.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalManagement.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string MakesEndPoint = $"{Prefix}/makes";
+        public static readonly string ModelsEndPoint = $"{Prefix}/models";
+        public static readonly string ColoursEndPoint = $"{Prefix}/colours";
+        public static readonly string VehiclesEndPoint = $"{Prefix}/vehicles";
+        public static readonly string CustomersEndPoint = $"{Prefix}/customers";
+        public static readonly string BookingsEndPoint = $"{Prefix}/bookings";
+
+    }
+}
